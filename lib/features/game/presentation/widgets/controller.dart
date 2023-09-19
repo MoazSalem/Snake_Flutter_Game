e2ee1a5541207e46fc727d2ed0a310f3a034fa44
MoazSalem <moaztalaat6@gmail.com>
2023-09-19 13:31:55 +0300

    controller view enhancements

--- lib/features/game/presentation/widgets/controller.dart
+++ lib/features/game/presentation/widgets/controller.dart
@@ -5,72 +5,77 @@ class Controller extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.sizeOf(context).height * 0.2,
-      child: Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0),
-                    side: BorderSide(color: Theme.of(context).colorScheme.primary, width: 0.2),
-                  )),
-                  minimumSize: MaterialStateProperty.all<Size>(const Size(1000, 1000))),
-              child: const Icon(Icons.arrow_back_rounded),
+    return Padding(
+      padding: const EdgeInsets.only(left: 24.0, right: 24.0, bottom: 12.0),
+      child: SizedBox(
+        height: MediaQuery.sizeOf(context).height * 0.2,
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    )),
+                    minimumSize: MaterialStateProperty.all<Size>(
+                        const Size(double.infinity, double.infinity))),
+                child: const Icon(Icons.arrow_back_rounded),
+              ),
             ),
-          ),
-          Expanded(
-            flex: 3,
-            child: Column(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(0),
-                          side:
-                              BorderSide(color: Theme.of(context).colorScheme.primary, width: 0.2),
-                        )),
-                        minimumSize: MaterialStateProperty.all<Size>(const Size(1000, 1000))),
-                    child: const Icon(Icons.arrow_upward),
+            Expanded(
+              flex: 3,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0),
+                          )),
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color.fromRGBO(27, 37, 35, 1.0)),
+                          minimumSize: MaterialStateProperty.all<Size>(
+                              const Size(double.infinity, double.infinity))),
+                      child: const Icon(Icons.arrow_upward),
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(0),
-                          side:
-                              BorderSide(color: Theme.of(context).colorScheme.primary, width: 0.2),
-                        )),
-                        minimumSize: MaterialStateProperty.all<Size>(const Size(1000, 1000))),
-                    child: const Icon(Icons.arrow_downward),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0),
+                          )),
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color.fromRGBO(27, 37, 35, 0.8)),
+                          minimumSize: MaterialStateProperty.all<Size>(
+                              const Size(double.infinity, double.infinity))),
+                      child: const Icon(Icons.arrow_downward),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0),
-                    side: BorderSide(color: Theme.of(context).colorScheme.primary, width: 0.2),
-                  )),
-                  minimumSize: MaterialStateProperty.all<Size>(const Size(1000, 1000))),
-              child: const Icon(Icons.arrow_forward_rounded),
+            Expanded(
+              flex: 2,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    )),
+                    minimumSize: MaterialStateProperty.all<Size>(
+                        const Size(double.infinity, double.infinity))),
+                child: const Icon(Icons.arrow_forward_rounded),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
