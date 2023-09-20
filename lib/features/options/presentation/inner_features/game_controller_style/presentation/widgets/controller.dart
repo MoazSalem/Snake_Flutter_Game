f891308eeca38b830e175025c1798d02aeffec80
MoazSalem <moaztalaat6@gmail.com>
2023-09-20 15:02:53 +0300

    game controller style view done

---
+++ lib/features/options/presentation/inner_features/game_controller_style/presentation/widgets/controller.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:snake/core/utils/service_locator.dart';
+import 'package:snake/core/utils/styles.dart';
+
+class Controller extends StatelessWidget {
+  final int style;
+  const Controller({Key? key, required this.style}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 24.0, right: 24.0, bottom: 24.0, top: 6.0),
+      child: SizedBox(
+        height: MediaQuery.sizeOf(context).height * 0.15,
+        child: Row(
+          children: [
+            Expanded(
+              flex: style == 2 ? 1 : 2,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: Styles.flatButton,
+                child: const Icon(Icons.arrow_back_rounded),
+              ),
+            ),
+            style == 2
+                ? Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: Styles.flatButton.copyWith(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              getIt.get<ColorScheme>().background.withOpacity(0.7))),
+                      child: const Icon(Icons.arrow_upward),
+                    ),
+                  )
+                : Expanded(
+                    flex: 3,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: Styles.flatButton.copyWith(
+                                backgroundColor: MaterialStateProperty.all<Color>(
+                                    getIt.get<ColorScheme>().background.withOpacity(0.7))),
+                            child: const Icon(Icons.arrow_upward),
+                          ),
+                        ),
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: Styles.flatButton.copyWith(
+                                backgroundColor: MaterialStateProperty.all<Color>(
+                                    getIt.get<ColorScheme>().background.withOpacity(0.4))),
+                            child: const Icon(Icons.arrow_downward),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+            style == 2
+                ? Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: Styles.flatButton.copyWith(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              getIt.get<ColorScheme>().background.withOpacity(0.4))),
+                      child: const Icon(Icons.arrow_downward),
+                    ),
+                  )
+                : Container(),
+            Expanded(
+              flex: style == 2 ? 1 : 2,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: Styles.flatButton,
+                child: const Icon(Icons.arrow_forward_rounded),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/options/presentation/inner_features/game_controller_style/presentation/widgets/game_controller_style_view_body.dart
+++ lib/features/options/presentation/inner_features/game_controller_style/presentation/widgets/game_controller_style_view_body.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'controller.dart';
 
-class GameControllerStyleViewBody extends StatelessWidget {
+class GameControllerStyleViewBody extends StatefulWidget {
   const GameControllerStyleViewBody({Key? key}) : super(key: key);
 
   @override
+  State<GameControllerStyleViewBody> createState() => _GameControllerStyleViewBodyState();
+}
+
+class _GameControllerStyleViewBodyState extends State<GameControllerStyleViewBody> {
+  late int _style;
+
+  @override
+  void initState() {
+    super.initState();
+    _style = 1;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        InkWell(
+          onTap: () {
+            setState(() {
+              _style = 1;
+            });
+          },
+          child: Column(
+            children: [
+              ListTile(
+                title: const Text("Style 1"),
+                trailing: Radio(value: _style, groupValue: 1, onChanged: (value) {}),
+              ),
+              const Controller(style: 1),
+            ],
+          ),
+        ),
+        InkWell(
+          onTap: () {
+            setState(() {
+              _style = 2;
+            });
+          },
+          child: Column(
+            children: [
+              ListTile(
+                title: const Text("Style 2"),
+                trailing: Radio(value: _style, groupValue: 2, onChanged: (value) {}),
+              ),
+              const Controller(style: 2),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
